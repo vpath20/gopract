ca1f0a8d557d7b72bbe3a1e86cd9597ace0419b1
Vikrant Pathak <vikrantpathak10@outlook.com>
2024-12-17 16:16:17 +0530

    Add the maps file

--- 8slice/go.mod
+++ 8slice/go.mod
@@ -1,3 +1,3 @@
-module slices
+module slice
 
-go 1.22.2
+go 1.22.3
--- 8slice/main.go
+++ 8slice/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("Welcome to slices in golang")
@@ -8,7 +11,28 @@ func main() {
 	var fruitsList = []string{"mangoes", "banana", "guava"}
 	fmt.Printf("Types of fruitlist is %T\n ", fruitsList)
 
-	fruitsList = append(fruitsList[1:])
+	fruitsList = append(fruitsList[:3])
 	fmt.Println(fruitsList)
 
+	highScores := make([]int, 4)
+
+	highScores[0] = 234
+	highScores[1] = 945
+	highScores[2] = 465
+	highScores[3] = 867
+
+	highScores = append(highScores, 624, 320, 123)
+	fmt.Println(highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+
+	//how to remove a value from slices based on index
+
+	var subjects = []string{"math", "english", "biology", "physics"}
+	fmt.Println(subjects)
+
+	var index int = 2
+	subjects = append(subjects[:index], subjects[index+1:]...)
+	fmt.Println(subjects)
 }
